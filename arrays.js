7248818a285aff6ef5259f2d72405a6147e63b1a
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 14:51:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,13 @@ function destructivelyAddElementToBeginingOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
