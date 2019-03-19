9b99a8fe80dae9756f2b2006b7e5fb0ca40f2157
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 14:44:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginingOfArray(array, element) {
-  var newArray = [$(element), ...$(array)];
+  var newArray = [element, ...array];
   return newArray
 }
 
